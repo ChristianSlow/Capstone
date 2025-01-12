5e5034f1df76e3edf3e73872c83d167e88d79a27
ChristianSlow <christianslow.cpsu@gmail.com>
2025-01-12 21:55:15 +0800

    okay

--- src/pages/admin/Course&Subjects.vue
+++ src/pages/admin/Course&Subjects.vue
@@ -3,64 +3,29 @@ import { ref, onMounted } from 'vue';
 import { FilterMatchMode } from '@primevue/core/api';
 // import { useToast } from 'primevue/usetoast';
 import { ProductService } from '../../service/ProductService';
-import { collection, addDoc } from "firebase/firestore"; 
+import { collection, addDoc, getDocs } from "firebase/firestore"; 
 import { db } from '../../firebase';
 
 const data = ref ({
     course: '',
     major: '',
 })           
-// onMounted(() => {
-//     ProductService.getProducts().then((data) => (products.value = data));
-// });
+
 
 const fromDatabase =     [
-    {       
+            {       
                 course: 'Batchelor of Science in Business',
                 major: 'Major in Financial Management',
                 inventoryStatus:'',
 
             },
-            {
-                course: 'Batchelor of Science in Business',
-                major: 'Major in Marketing Management',
-                inventoryStatus:'',
-            },
-            {
-                course: 'College of Teacher Education',
-                major: 'Batchelor of Elementary Education',
-                inventoryStatus:'',
-            },
-            {
-                course: 'College of Teacher Education',
-                major: 'Batchelor of Secondary Education',
-                inventoryStatus:'',
-            },
-            {
-                course: 'College of Teacher Education',
-                major: 'Major in English',
-                inventoryStatus:'',
-            },
-            {
-                course: 'College of Teacher Education',
-                major: 'Major in Filipino',
-                inventoryStatus:'',
-            },
-            {
-                course: 'College of Teacher Education',
-                major: 'Major in Mathematics',
-                inventoryStatus:'',
-            },
-]
-
-onMounted(()=> {
-    products.value = fromDatabase
-})
-
-// const toast = useToast();
-const dt = ref();
-const products = ref();
-const productDialog = ref(false);
+        ]
+        
+        
+        // const toast = useToast();
+        const dt = ref();
+        const products = ref([]);
+        const productDialog = ref(false);
 const deleteProductDialog = ref(false);
 const deleteProductsDialog = ref(false);
 const product = ref({});
@@ -68,6 +33,24 @@ const selectedProducts = ref();
 const filters = ref({
     'global': {value: null, matchMode: FilterMatchMode.CONTAINS},
 });
+
+const isLoading = ref(false)
+const getData = async () => {
+    products.value = []
+    isLoading.value = true
+    const querySnapshot = await getDocs(collection(db, "Courses"));
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        products.value.push(doc.data());
+        console.log(doc.data());
+    });
+    isLoading.value = false
+};
+
+onMounted(() => {
+    getData();
+})
+
 const submitted = ref(false);
 const statuses = ref([
     {label: 'INSTOCK', value: 'instock'},
@@ -89,28 +72,30 @@ const hideDialog = () => {
     productDialog.value = false;
     submitted.value = false;
 };
-const saveProduct = () => {
-    addDoc(collection(db, "Courses"), data.value)
-    submitted.value = true;
+const saveProduct = async () => {
+    const result = await addDoc(collection(db, "Courses"), data.value)
+    console.log(result);
+    // submitted.value = true;
 
-    if (product?.value.name?.trim()) {
-        if (product.value.id) {
-            product.value.inventoryStatus = product.value.inventoryStatus.value ? product.value.inventoryStatus.value : product.value.inventoryStatus;
-            products.value[findIndexById(product.value.id)] = product.value;
-            toast.add({severity:'success', summary: 'Successful', detail: 'Product Updated', life: 3000});
-        }
-        else {
-            product.value.id = createId();
-            product.value.code = createId();
-            product.value.image = 'product-placeholder.svg';
-            product.value.inventoryStatus = product.value.inventoryStatus ? product.value.inventoryStatus.value : 'INSTOCK';
-            products.value.push(product.value);
-            toast.add({severity:'success', summary: 'Successful', detail: 'Product Created', life: 3000});
-        }
+    // if (product?.value.name?.trim()) {
+    //     if (product.value.id) {
+    //         product.value.inventoryStatus = product.value.inventoryStatus.value ? product.value.inventoryStatus.value : product.value.inventoryStatus;
+    //         products.value[findIndexById(product.value.id)] = product.value;
+    //         toast.add({severity:'success', summary: 'Successful', detail: 'Product Updated', life: 3000});
+    //     }
+    //     else {
+    //         product.value.id = createId();
+    //         product.value.code = createId();
+    //         product.value.image = 'product-placeholder.svg';
+    //         product.value.inventoryStatus = product.value.inventoryStatus ? product.value.inventoryStatus.value : 'INSTOCK';
+    //         products.value.push(product.value);
+    //         toast.add({severity:'success', summary: 'Successful', detail: 'Product Created', life: 3000});
+    //     }
 
-        productDialog.value = false;
-        product.value = {};
-    }
+    //     product.value = {};
+    // }
+    productDialog.value = false;
+    getData()
 };
 const editProduct = (prod) => {
     product.value = {...prod};
@@ -198,13 +183,14 @@ const getStatusLabel = (status) => {
                 v-model:selection="selectedProducts"
                 :value="products"
                 dataKey="id"
-                :paginator="true"
+                :loading="isLoading"
                 :rows="10"
                 :filters="filters"
-                paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+                >
+                <!-- :paginator="true" -->
+                <!-- paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
                 :rowsPerPageOptions="[5, 10, 25]"
-                currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
-            >
+                currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products" -->
                 <template #header>
                     <div class="flex flex-wrap gap-2 items-center ">
                         <IconField>
@@ -238,13 +224,13 @@ const getStatusLabel = (status) => {
                 <img v-if="product.image" :src="`https://primefaces.org/cdn/primevue/images/product/${product.image}`" :alt="product.image" class="block m-auto pb-4" />
                 <div>
                     <label for="course" class="block font-bold mb-3">Course</label>
-                    <InputText id="course" v-model.trim="product.course" required="true" autofocus :invalid="submitted && !product.course" fluid />
-                    <small v-if="submitted && !product.course" class="text-red-500">Course is required.</small>
+                    <InputText id="course" v-model="data.course" required="true" autofocus :invalid="submitted && !product.course" fluid />
+                    <small v-if="submitted && !data.course" class="text-red-500">Course is required.</small>
                 </div>
                 <div>
                     <label for="major" class="block font-bold mb-3">Major</label>
-                    <InputText id="major" v-model.trim="product.major" required="true" autofocus :invalid="submitted && !product.major" fluid />
-                    <small v-if="submitted && !product.major" class="text-red-500">Major is required.</small>   
+                    <InputText id="major" v-model="data.major" required="true" autofocus :invalid="submitted && !product.major" fluid />
+                    <small v-if="submitted && !data.major" class="text-red-500">Major is required.</small>   
                 </div>
                 <!-- <div>
                     <label for="description" class="block font-bold mb-3">Description</label>
