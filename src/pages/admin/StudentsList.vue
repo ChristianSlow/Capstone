adb783847a64a42ff23afdd8454ddb406b580b2a
ChristianSlow <christianslow.cpsu@gmail.com>
2025-01-14 00:44:35 +0800

    kaya na

--- src/pages/admin/StudentsList.vue
+++ src/pages/admin/StudentsList.vue
@@ -1,76 +1,30 @@
-<template>
-    <main class="p-4 md:ml-64 h-auto pt-20 bg-red-800">
-            <h1 class="text-center text-4xl font-bold bg-gray-200 border-b border-gray-200 p-3">STUDENTS LIST</h1>
-            <!-- <template> -->
-        <!-- <ThemeSwitcher /> -->
-        <div class="card">
-            <DataTable v-model:filters="filters" v-model:selection="selectedCustomer" :value="customers"
-                    stateStorage="session" stateKey="dt-state-demo-session" paginator :rows="5" filterDisplay="menu"
-                    selectionMode="single" dataKey="id" :globalFilterFields="['name', 'country.name', 'representative.name', 'status']" tableStyle="min-width: 50rem">
-                <template #header>
-                    <IconField>
-                    <InputIcon>
-                        <i class="pi pi-search" />
-                    </InputIcon>
-                    <InputText v-model="filters['global'].value" placeholder="Global Search" />
-                </IconField>
-                </template>
-                <Column field="name" header="Name" sortable style="width: 25%">
-                    <template #filter="{ filterModel }">
-                        <InputText v-model="filterModel.value" type="text" placeholder="Search by name" />
-                    </template>
-                </Column>
-                <Column header="Country" sortable sortField="country.name" filterField="country.name" filterMatchMode="contains" style="width: 25%">
-                    <template #body="{ data }">
-                        <div class="flex items-center gap-2">
-                            <img alt="flag" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png" :class="`flag flag-${data.country.code}`" style="width: 24px" />
-                            <span>{{ data.country.name }}</span>
-                        </div>
-                    </template>
-                    <template #filter="{ filterModel }">
-                        <InputText v-model="filterModel.value" type="text" placeholder="Search by country" />
-                    </template>
-                </Column>
-                <Column header="Representative" sortable sortField="representative.name" filterField="representative" :showFilterMatchModes="false" :filterMenuStyle="{ width: '14rem' }" style="width: 25%">
-                    <template #body="{ data }">
-                        <div class="flex items-center gap-2">
-                            <img :alt="data.representative.name" :src="`https://primefaces.org/cdn/primevue/images/avatar/${data.representative.image}`" style="width: 32px" />
-                            <span>{{ data.representative.name }}</span>
-                        </div>
-                    </template>
-                    <template #filter="{ filterModel }">
-                        <MultiSelect v-model="filterModel.value" :options="representatives" optionLabel="name" placeholder="Any">
-                            <template #option="slotProps">
-                                <div class="flex items-center gap-2">
-                                    <img :alt="slotProps.option.name" :src="`https://primefaces.org/cdn/primevue/images/avatar/${slotProps.option.image}`" style="width: 32px" />
-                                    <span>{{ slotProps.option.name }}</span>
-                                </div>
-                            </template>
-                        </MultiSelect>
-                    </template>
-                </Column>
-                <Column field="status" header="Status" sortable filterMatchMode="equals" style="width: 25%">
-                    <template #body="{ data }">
-                        <Tag :value="data.status" :severity="getSeverity(data.status)" />
-                    </template>
-                    <template #filter="{ filterModel }">
-                        <Select v-model="filterModel.value" :options="statuses" placeholder="Select One" showClear>
-                            <template #option="slotProps">
-                                <Tag :value="slotProps.option" :severity="getSeverity(slotProps.option)" />
-                            </template>
-                        </Select>
-                    </template>
-                </Column>
-                <template #empty> No customers found. </template>
-            </DataTable>
-        </div>
-    </main>
-</template>
-
 <script setup>
 import { ref, onMounted } from 'vue';
 import { CustomerService } from '../../service/CustomerService';
 import { FilterMatchMode, FilterOperator } from '@primevue/core/api';
+import { collection, addDoc, getDocs, doc, deleteDoc } from "firebase/firestore"; 
+import { db } from '../../firebase';
+
+
+
+const students = ref([])
+
+const isLoading = ref(false)
+const getData = async () => {
+    students.value = []
+    isLoading.value = true
+    const querySnapshot = await getDocs(collection(db, "StudentInformation"));
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        students.value.push({id: doc.id, ...doc.data()});
+        console.log(doc.data());
+    });
+    isLoading.value = false
+};
+
+onMounted(() => {
+    getData();
+})
 
 const customers = ref();
 const selectedCustomer = ref();
@@ -121,3 +75,66 @@ const getSeverity = (status) => {
 };
 
 </script>
+<template>
+    <main class="p-4 md:ml-64 h-auto pt-20 bg-red-800">
+            <h1 class="text-center text-4xl font-bold bg-gray-200 border-b border-gray-200 p-3">STUDENTS LIST</h1>
+            <!-- <template> -->
+        <!-- <ThemeSwitcher /> -->
+        <div class="card">
+            <DataTable v-model:filters="filters" v-model:selection="selectedCustomer" :value="students"
+                    stateStorage="session" stateKey="dt-state-demo-session" paginator :rows="5" filterDisplay="menu"
+                    selectionMode="single" dataKey="id" :globalFilterFields="['name', 'country.name', 'representative.name', 'status']" tableStyle="min-width: 50rem">
+                <template #header>
+                    <IconField>
+                    <InputIcon>
+                        <i class="pi pi-search" />
+                    </InputIcon>
+                    <InputText v-model="filters['global'].value" placeholder="Search name here" />
+                </IconField>
+                </template>
+                <Column field="fname" header="Students Name" style="width: 25%">
+                    <template #body="{ data }">
+                        <div class="flex items-center gap-2">
+                            <span>{{ data.fname }}  {{ data.mname }} {{ data.lname }}</span>
+                        </div>
+                    </template>
+                </Column>
+                <Column header="Course" style="width: 25%">
+                    <template #body="{ data }">
+                        <div class="flex items-center gap-2">
+                            <span>{{ data.selectedCourse }}</span>
+                        </div>
+                    </template>
+                </Column>
+                <Column field="major" header="Major" style="width: 25%">
+                </Column>
+                <!-- <Column header="Major" sortField="representative.name" filterField="representative" :showFilterMatchModes="false" :filterMenuStyle="{ width: '14rem' }" style="width: 25%">
+                    <template #body="{ data }">
+                        <div class="flex items-center gap-2">
+                            <img :alt="data.representative.name" :src="`https://primefaces.org/cdn/primevue/images/avatar/${data.representative.image}`" style="width: 32px" />
+                            <span>{{ data.representative.name }}</span>
+                        </div>
+                    </template>
+                    <template #filter="{ filterModel }">
+                        <MultiSelect v-model="filterModel.value" :options="representatives" optionLabel="name" placeholder="Any">
+                            <template #option="slotProps">
+                                <div class="flex items-center gap-2">
+                                    <img :alt="slotProps.option.name" :src="`https://primefaces.org/cdn/primevue/images/avatar/${slotProps.option.image}`" style="width: 32px" />
+                                    <span>{{ slotProps.option.name }}</span>
+                                </div>
+                            </template>
+                        </MultiSelect>
+                    </template>
+                </Column>
+            -->
+                <Column header="Action" style="width: 25%">
+                    <template #body="slotProps">
+                        <Button icon="pi pi-pencil" outlined rounded class="mr-2" />
+                        <Button icon="pi pi-trash" outlined rounded severity="danger"/>
+                    </template>
+                </Column> 
+                <template #empty> No customers found. </template>
+            </DataTable>
+        </div>
+    </main>
+</template>
